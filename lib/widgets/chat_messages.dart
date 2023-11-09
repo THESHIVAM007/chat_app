74a5c09f66f3649a2781d3d79f63947e825bead1
Shivam Choudhary <96073230+THESHIVAM007@users.noreply.github.com>
2023-11-09 23:25:40 +0530

    chat-message-styling-done

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,11 +1,15 @@
+import 'package:chat_app/widgets/message_bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
+
   const ChatMessages({super.key});
 
   @override
   Widget build(BuildContext context) {
+  final authenticatedUser = FirebaseAuth.instance.currentUser!;
     return StreamBuilder(
         stream: FirebaseFirestore.instance.collection('chat').orderBy('createdAt',descending: true).snapshots(),
         builder: (ctx, chatSnapshot) {
@@ -29,8 +33,22 @@ class ChatMessages extends StatelessWidget {
             padding: const EdgeInsets.only(bottom: 40,left: 13,right: 13),
             reverse: true,
             itemCount: loadedMessages.length,
-            itemBuilder: (ctx, index) =>
-                Text(loadedMessages[index].data()['text']),
+            itemBuilder: (ctx, index) {
+
+            final chatMessage = loadedMessages[index].data();
+            final nextMessage = index+1 < loadedMessages.length ? loadedMessages[index+1].data(): null;
+            final currentMessageUserId = chatMessage['userId'];
+            final nextMessageUserId = nextMessage!=null ? nextMessage['userId'] : null;
+            final nextUserIsSame = nextMessageUserId==currentMessageUserId;
+
+            if(nextUserIsSame){
+              return MessageBubble.next(message: chatMessage['text'], isMe: authenticatedUser.uid == currentMessageUserId );
+
+            }else{
+              return MessageBubble.first(userImage: chatMessage['userImage'], username: chatMessage['username'], message: chatMessage['text'], isMe:authenticatedUser.uid == currentMessageUserId );
+            }
+            }
+                
           );
         });
   }
